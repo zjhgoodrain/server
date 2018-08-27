c257eede2f0cea19abd1f3d4cf76bf749655e016
Robin Appelman <robin@icewind.nl>
2018-08-27 14:55:24 +0200

    A dav plugin to allow pagination of multipart responses
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- apps/dav/lib/Server.php
+++ apps/dav/lib/Server.php
@@ -54,6 +54,7 @@ use OCA\DAV\Connector\Sabre\QuotaPlugin;
 use OCA\DAV\Files\BrowserErrorPagePlugin;
 use OCA\DAV\Connector\Sabre\AnonymousOptionsPlugin;
 use OCA\DAV\Files\LazySearchBackend;
+use OCA\DAV\Paginate\PaginatePlugin;
 use OCA\DAV\SystemTag\SystemTagPlugin;
 use OCA\DAV\Upload\ChunkingPlugin;
 use OCP\IRequest;
@@ -180,6 +181,7 @@ class Server {
 
 		$this->server->addPlugin(new CopyEtagHeaderPlugin());
 		$this->server->addPlugin(new ChunkingPlugin());
+		$this->server->addPlugin(\OC::$server->query(PaginatePlugin::class));
 
 		// allow setup of additional plugins
 		$dispatcher->dispatch('OCA\DAV\Connector\Sabre::addPlugin', $event);
